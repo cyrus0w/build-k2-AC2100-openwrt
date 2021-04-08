7e839aaac55593fe265a5a15a904b1043531af16
cyrus0w <64672678+cyrus0w@users.noreply.github.com>
2021-04-08 22:20:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,33 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+###################################自定义路由基础设置#####################################
+sed -i "/exit 0/i\uci set network.lan.proto='static'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set network.lan.ipaddr='192.168.2.1'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set network.lan.gateway='192.168.1.1'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set network.lan.dns='192.168.1.1'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set network.lan.netmask='255.255.255.0'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+
+sed -i "/exit 0/i\uci set network.wan.proto='static'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set network.wan.ipaddr='192.168.1.3'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set network.wan.gateway='192.168.1.1'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set network.wan.dns='192.168.1.1'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set network.wan.netmask='255.255.255.0'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+
+sed -i "/exit 0/i\uci set network.wan6.proto=none" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings   #关掉wan6
+
+sed -i "/exit 0/i\uci set wireless.default_radio0.ssid='Airport_5G'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set wireless.default_radio0.encryption='sae-mixed'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set wireless.default_radio0.key='308fuckwj..'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+
+sed -i "/exit 0/i\uci set wireless.default_radio1.ssid='Airport'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set wireless.default_radio1.encryption='sae-mixed'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci set wireless.default_radio1.key='308fuckwj..'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+
+sed -i "/exit 0/i\uci set uci set dhcp.@dnsmasq[0].filter_aaaa='0'" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings   #关闭“禁止解析IPv6 DNS记录
+”（过滤掉 IPv6(AAAA) ，只返回 IPv4 DNS 域名记录）
+
+sed -i "/exit 0/i\uci commit network" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci commit wireless" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\uci commit dhcp" /workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+########################################################################################
